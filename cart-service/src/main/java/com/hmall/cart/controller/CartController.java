6cdeb235a5a9aa4491a2c8ae70ec7e7d44f0b54e
“songwaimaideyasuo” <1178347787@qq.com>
2023-12-22 23:54:16 +0800

    网关传递用户

--- cart-service/src/main/java/com/hmall/cart/controller/CartController.java
+++ cart-service/src/main/java/com/hmall/cart/controller/CartController.java
@@ -43,7 +43,8 @@ public class CartController {
 
     @ApiOperation("查询购物车列表")
     @GetMapping
-    public List<CartVO> queryMyCarts(){
+    public List<CartVO> queryMyCarts(@RequestHeader(value = "user-info", required = false) String userInfo){
+        System.out.println("userInfo = " + userInfo);
         return cartService.queryMyCarts();
     }
     @ApiOperation("批量删除购物车中商品")
--- hm-gateway/src/main/java/com/hmall/gateway/filters/AuthGlobalFilter.java
+++ hm-gateway/src/main/java/com/hmall/gateway/filters/AuthGlobalFilter.java
@@ -56,10 +56,14 @@ public class AuthGlobalFilter implements GlobalFilter, Ordered {
             return response.setComplete();
         }
 
-        // TODO 5.如果有效，传递用户信息
-        System.out.println("userId = " + userId);
+        // 5.如果有效，传递用户信息
+        String userInfo = userId.toString();
+        ServerWebExchange swe = exchange.mutate()
+                .request(builder -> builder.header("user-info", userInfo))
+                .build();
+
         // 6.放行
-        return chain.filter(exchange);
+        return chain.filter(swe);
     }
 
     private boolean isExclude(String antPath) {
